a9611783919e784ca6cc74f5bd33debf41dd9c11
julia-palant <61748302+julia-palant@users.noreply.github.com>
2020-04-13 15:57:20 +0300

    Add files via upload

---
+++ python_basics_homeworks/Homeworks_les1/Task_4.py
@@ -0,0 +1,31 @@
+"""
+Пользователь вводит целое положительное число.
+Найдите самую большую цифру в числе.
+Для решения используйте цикл while и арифметические операции.
+"""
+
+while True:
+    number = input('Введите целое число: ')
+    if number.isdigit():
+        number = int(number)
+        if number > 0:
+            break
+    else:
+        print('Необходимо ввести целое положительное число')
+
+# I'm deeply ashamed of anything you see bellow this comment, sorry. Your student.
+# I'm still figuring it out...
+number = str(number)
+i = len(number)
+i == i - 1
+while i > 0:
+
+
+    int(number[():()])
+    if i > len(number)
+        break
+
+
+print(i)
+print(number)
+#while True:
